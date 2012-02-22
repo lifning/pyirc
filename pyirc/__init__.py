200535fff610b199b7ed0b481edc0f5e167953bc
Darren Alton <dalton@stevens.edu>
2012-02-22 00:38:29 -0500

    cleaned up imports a bit and dual python 2 and 3 support.

--- pyirc/__init__.py
+++ pyirc/__init__.py
@@ -1,3 +1,7 @@
-from IRCConnection import IRCConnection
-from IRCChannel import IRCChannel
+try:
+	from .IRCConnection import IRCConnection
+	from .IRCChannel import IRCChannel
+except:
+	from IRCConnection import IRCConnection
+	from IRCChannel import IRCChannel
 
