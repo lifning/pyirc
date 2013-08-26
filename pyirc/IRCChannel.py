a16da4afdf132b818b0557c5df8f39a95a8e3e36
lif <lifning@localhost>
2013-08-25 21:48:04 -0700

    use asyncore and asynchat, instead of the messy multiprocessing that was done before.  add a simple example client.

--- pyirc/IRCChannel.py
+++ pyirc/IRCChannel.py
@@ -1,17 +1,17 @@
+import io
+
 try:
 	from .IRCConnection import *
 except:
 	from IRCConnection import *
 
-class IRCChannel(RawIOBase):
-	def __init__(self, channel, connection, pipename):
+class IRCChannel(io.RawIOBase):
+	def __init__(self, channel, connection, readpipe):
 		self.connection = connection
 		self.channel = channel
 		self.users = set()
 		connection.write('JOIN %s' % channel)
-
-		fdr = os.open(pipename, os.O_RDONLY|os.O_NONBLOCK)
-		self.mypipe = os.fdopen(fdr, 'rb')
+		self.mypipe = os.fdopen(readpipe, 'rb')
 
 	def readline(self):
 		return self.mypipe.readline().decode(self.connection.encoding, 'replace')
@@ -20,23 +20,20 @@ class IRCChannel(RawIOBase):
 		return self.mypipe.read().decode(self.connection.encoding, 'replace')
 
 	def write(self, msg):
-		self.connection.send('PRIVMSG %s :%s' % (self.channel, msg))
+		self.connection.write('PRIVMSG %s :%s' % (self.channel, msg))
 
 	def close(self):
 		self.mypipe.close()
-		os.unlink('/tmp/pyirc-{}-{}-{}'.format(self.connection.serveraddress,
-		                                       self.channel,
-		                                       self.connection.mynick))
-		self.connection.send('PART %s' % self.channel)
+		self.connection.write('PART %s' % self.channel)
 
 	def kick(self, who):
-		self.connection.send('KICK %s %s' % (self.channel, who))
+		self.connection.write('KICK %s %s' % (self.channel, who))
 
 	# misc. filelike-isms
 	def fileno(self): return self.mypipe.fileno()
 	def seek(self, n): raise IOError("not a seekable object")
 	def tell(self): raise IOError("not a seekable object")
-	def truncate(self): raise IOError("not a seekable object")
+	def truncate(self): raise IOError("not a truncatable object")
 	def readable(self): return True
 	def writable(self): return True
 	def seekable(self): return False
